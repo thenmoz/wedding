8e63705f42fdac9670caa5abb0312947c1a5acbd
NMO <34682099+thenmoz@users.noreply.github.com>
2024-09-30 16:10:05 +0700

    Info complete

--- src/components/WeddingInfo.tsx
+++ src/components/WeddingInfo.tsx
@@ -1,42 +1,50 @@
 import React from 'react';
 import WeddingInfo1 from "../assets/images/wedding-info-1.png";
-import WeddingInfo2 from "../assets/images/wedding-info-2.png";
+import FlowerLeft from "../assets/images/flower-left.png";
+import FlowerRight from "../assets/images/flower-right.png";
 
 const WeddingInfo: React.FC = () => {
   return (
-    <div className="bg-[#E3CAA5] p-8 flex items-center justify-center">
+    <div className="bg-[#E3CAA5] flex items-center justify-center">
       {/* Left Side - Large Image */}
-      <div className="w-full flex justify-center">
+      <div className="w-1/2 p-6 flex justify-center">
         <img
           src={WeddingInfo1}
           alt="Wedding Couple"
-          className="w-full md:w-3/4 lg:w-1/2 object-cover rounded-lg shadow-lg h-auto"
+          className="w-full md:w-3/4 lg:w-1/2 object-cover h-auto"
         />
       </div>
 
       {/* Right Side - Text and Images Section */}
-      <div className="w-full md:w-1/2 pl-0 md:pl-8">
+      <div className="w-1/2 mt-6">
         {/* Top Section with Date, Venue, and Time */}
         <div className="text-center mb-8">
-          <h2 className="sm:text-20 md:text-4xl lg:text-5xl font-bold font-lustria text-[#705538]">30 . 11 . 2024</h2>
-          <p className="sm:text-14 md:text-4xl lg:text-5xl font-normal font-lustria text-[#705538] mt-2">at Kensington English Garden Resort Khaoyai</p>
-          <p className="sm:text-14 md:text-4xl lg:text-5xl font-normal font-lustria text-[#705538] mt-6">เวลา 17:30 น.</p>
+          <p className="text-20 md:text-20 font-lustria text-[#6C4E31]">30 . 11 . 2024</p>
+          <p className="text-14 font-libreCaslon text-[#6C4E31]">at Kensington English Garden</p>
+          <p className="text-14 font-libreCaslon text-[#6C4E31]">Resort Khaoyai</p>
+          <p className="text-14 font-libreCaslon text-[#6C4E31] mt-6">เวลา 17:30 น.</p>
         </div>
 
         {/* Theme Section */}
         <div className="text-center">
-          <h3 className="text-xl font-libreCaslon text-[#705538]">THEME</h3>
-          <div className="flex justify-center space-x-4 mt-2">
-            <div className="w-8 h-8 rounded-full bg-[#ECC9B3]"></div>
-            <div className="w-8 h-8 rounded-full bg-[#FAE6CB]"></div>
-            <div className="w-8 h-8 rounded-full bg-[#F2DEC3]"></div>
-            <div className="w-8 h-8 rounded-full bg-[#D3B698]"></div>
+          <p className="text-13 font-semibold font-libreCaslon text-[#705538]">DRESS CODE</p>
+          <div className="flex justify-center relative">
+            <div className="w-7 h-7 bg-[#FFD0D0] rounded-full -mr-2 z-50"></div>
+            <div className="w-7 h-7 bg-[#F6B5B5] rounded-full -mr-2 z-40"></div>
+            <div className="w-7 h-7 bg-[#FFDBB5] rounded-full -mr-2 z-30"></div>
+            <div className="w-7 h-7 bg-[#CEAB93] rounded-full -mr-2 z-20"></div>
+            <div className="w-7 h-7 bg-[#AD8B73] rounded-full "></div>
           </div>
-          <div>
+        </div>
+        <div className="w-full">
+          <div className='flex justify-between'>
             <img
-              src={WeddingInfo2}
-              alt="Wedding Couple"
-              className="w-full md:w-3/4 lg:w-1/2 object-cover rounded-lg shadow-lg h-auto"
+              src={FlowerLeft}
+              className='object-cover h-auto'
+            />
+            <img
+              src={FlowerRight}
+              className='object-cover h-auto'
             />
           </div>
         </div>
