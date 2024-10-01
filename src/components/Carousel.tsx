31ea9b4504509ab690fb8f2a38a86faf9436cdfa
NMO <34682099+thenmoz@users.noreply.github.com>
2024-10-01 16:51:18 +0700

    Add gradient bottom on carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -3,7 +3,10 @@ import WeddingCarousel from '../assets/images/wedding-carousel.png';
 
 const Carousel: React.FC = () => {
     return (
-        <img src={WeddingCarousel} />
+        <div className="relative">
+            <img src={WeddingCarousel} alt="Wedding Carousel" className="w-full h-auto" />
+            <div className="absolute inset-x-0 bottom-0 h-1/4 bg-gradient-to-t from-white to-transparent"></div>
+        </div>
     )
 }
 
