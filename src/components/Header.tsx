c860a42f105989241dbecfad79f9750bb83251d6
NMO <34682099+thenmoz@users.noreply.github.com>
2024-10-04 13:17:00 +0700

    change font color on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,9 +4,9 @@ const Header: React.FC = () => {
 
     return (
         <div className="flex items-center justify-center my-6">
-            <div className="flex-grow border-t sm:border-t-2 border-[#E3CAA5]"></div>
-            <p className="text-20 sm:text-32 font-normal mx-4 tracking-wider font-libreCaslon text-[#E3CAA5]">CHANUT & WARISSARA</p>
-            <div className="flex-grow border-t sm:border-t-2 border-[#E3CAA5]"></div>
+            <div className="flex-grow border-t sm:border-t-2 border-[#6C4E31]"></div>
+            <p className="text-20 sm:text-32 font-normal mx-4 tracking-wider font-libreCaslon text-[#6C4E31]">CHANUT & WARISSARA</p>
+            <div className="flex-grow border-t sm:border-t-2 border-[#6C4E31]"></div>
         </div>
     );
 }
