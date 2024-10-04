824769fea643a4781fd28558b8f07739d3af2908
NMO <34682099+thenmoz@users.noreply.github.com>
2024-10-04 17:39:56 +0700

    Change img responsive size

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -19,13 +19,13 @@ const Timeline: React.FC = () => {
     return (
         <div className="w-full mx-auto bg-[#6C4E31] relative">
             <div className="absolute bottom-0 left-0">
-                <img src={FlowerLeft} className="max-h-36 opacity-35" />
+                <img src={FlowerLeft} className="max-h-24 sm:max-h-56 opacity-35" />
             </div>
             <div className="absolute bottom-0 right-0">
-                <img src={FlowerRightSt} className="max-h-36 opacity-35" />
+                <img src={FlowerRightSt} className="max-h-24 sm:max-h-56 opacity-35" />
             </div>
             <div className="absolute bottom-0 right-0">
-                <img src={FlowerRightNd} className="max-h-36" />
+                <img src={FlowerRightNd} className="max-h-24 sm:max-h-56" />
             </div>
 
             <p className="text-[#E3CAA5] font-libreBodoni px-4 py-4 text-20 sm:text-32 tracking-wider">
