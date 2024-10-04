ef6bde9d7b6a2e3feb5ccd0e2b7bc476c817fc67
NMO <Chanut.Wongtripipat@gmail.com>
2024-10-04 13:00:19 +0700

    Change color on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,9 +4,9 @@ const Header: React.FC = () => {
 
     return (
         <div className="flex items-center justify-center my-6">
-            <div className="flex-grow border-t border-black"></div>
-            <p className="text-custom-20 font-normal mx-4 tracking-wider font-libreCaslon">CHANUT & WARISSARA</p>
-            <div className="flex-grow border-t border-black"></div>
+            <div className="flex-grow border-t sm:border-t-2 border-[#E3CAA5]"></div>
+            <p className="text-20 sm:text-32 font-normal mx-4 tracking-wider font-libreCaslon text-[#E3CAA5]">CHANUT & WARISSARA</p>
+            <div className="flex-grow border-t sm:border-t-2 border-[#E3CAA5]"></div>
         </div>
     );
 }
