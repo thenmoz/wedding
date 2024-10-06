7ccb8cf99b5033e7ede68bbf4fd5f60a8632d2ac
NMO <Chanut.Wongtripipat@gmail.com>
2024-10-06 23:25:43 +0700

    add a sample carousel

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,24 @@ import Carousel from "./components/Carousel";
 import WeddingInfo from "./components/WeddingInfo";
 import React from "react";
 import Timeline from "./components/Timeline";
+import ImageSlider from "./components/ImageSlider";
 
+import WeddingDummy1 from "./assets/images/wedding-dummy1.jpg";
+import WeddingDummy2 from "./assets/images/wedding-dummy2.jpg";
+
+const images = [WeddingDummy1, WeddingDummy2];
 const App: React.FC = () => {
-    return (
-        <>
-            <Header />
-            <Carousel />
-            <Countdown />
-            <WeddingInfo />
-            <Timeline />
-            <GoogleMapComponent />
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <Carousel />
+      <Countdown />
+      <ImageSlider images={images} />
+      <WeddingInfo />
+      <Timeline />
+      <GoogleMapComponent />
+    </>
+  );
 };
 
 export default App;
