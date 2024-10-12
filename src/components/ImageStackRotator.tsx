9acb58dd04bcbbe1eb7991f6c8d8458e6ba1d5e5
NMO <34682099+thenmoz@users.noreply.github.com>
2024-10-13 01:24:41 +0700

    set max h image

--- src/components/ImageStackRotator.tsx
+++ src/components/ImageStackRotator.tsx
@@ -12,15 +12,15 @@ const ImageStackRotator: React.FC<ImageStackRotatorProps> = ({ images }) => {
       setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
     }, 3000); // Change image every 3 seconds
 
-    return () => clearInterval(interval); // Cleanup interval on component unmount
+    return () => clearInterval(interval);
   }, [images.length]);
 
   const getRotation = (index: number) => {
-    if (index === 0) return ''; // No rotation for the first image
-    if (index === 1) return 'rotate-3'; // Rotate second image to the right (2 degrees)
-    if (index === 2) return '-rotate-3'; // Rotate third image to the left (2 degrees)
-    if (index === 3) return 'rotate-6'; // Rotate fourth image to the right (3 degrees)
-    if (index === 4) return '-rotate-6'; // Rotate fifth image to the left (3 degrees)
+    if (index === 0) return '';
+    if (index === 1) return 'rotate-3';
+    if (index === 2) return '-rotate-3';
+    if (index === 3) return 'rotate-6';
+    if (index === 4) return '-rotate-6';
     return '';
   };
 
@@ -31,7 +31,7 @@ const ImageStackRotator: React.FC<ImageStackRotatorProps> = ({ images }) => {
           key={index}
           src={image}
           alt={`Image ${index + 1}`}
-          className={`absolute transition-opacity duration-500 ease-in-out 
+          className={`absolute transition-opacity duration-500 ease-in-out max-h-80
             ${index <= currentImageIndex ? 'opacity-100' : 'opacity-0'} 
             ${getRotation(index)} transform`}
           style={{
